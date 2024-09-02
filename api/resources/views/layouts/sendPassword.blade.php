5fb44fc1a8c6d75d628195e4773b45afb6fd7f0b
baghdasaryangevorg97 <baghdasaryan.gevorg.97@gmail.com>
2024-09-03 01:53:38 +0400

    change mail message

--- api/resources/views/layouts/sendPassword.blade.php
+++ api/resources/views/layouts/sendPassword.blade.php
@@ -1,19 +1,19 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Բարի գալուստ Aparto</title>
+    <title>Բարի գալուստ Prestige</title>
 </head>
 <body>
 
-   <p>Your email has been added to Aparto Admin Panel. Please sign in using your email address and the password indicated below: </p>
+   <p>Your email has been added to Prestige Admin Panel. Please sign in using your email address and the password indicated below: </p>
 
     <p>{{ $password }}</p>
 
-    <p>Link to the dashboard: <a href="https://aparto.am/login">https://aparto.am/login</a></p>
+    <p>Link to the dashboard: <a href="https://prestige-estate.am/login">https://prestige-estate.am/login</a></p>
 
     <p>After singing in, you can change your password from the Profile Setting page.</p>
 
    <p> Welcome to the Team!</p>
-   <p> Aparto </p>
+   <p> Prestige </p>
 </body>
 </html>
